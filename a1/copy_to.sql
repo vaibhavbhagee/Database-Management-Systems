01b6739eeac3d224d90560dcd3ac3564f01b58e6
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2018-01-29 10:06:16 +0530

    [a1] \copy used instead of COPY, working code

---
+++ a1/copy_to.sql
@@ -0,0 +1,6 @@
+\copy student to '/tmp/student.csv' DELIMITER ',';
+\copy teacher to '/tmp/teacher.csv' DELIMITER ',';
+\copy course to '/tmp/course.csv' DELIMITER ',';
+\copy teaches to '/tmp/teaches.csv' DELIMITER ',';
+\copy registers to '/tmp/registers.csv' DELIMITER ',';
+\copy section to '/tmp/section.csv' DELIMITER ',';
